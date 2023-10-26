7331571b27b677a70853160487dfd994e7bfd675
LONGMANKE <mburunjoroge0@gmail.com>
2023-10-26 12:40:58 +0300

    create database

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -2,34 +2,70 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import axios from 'axios';
+import avatar from './assets/profile.png'
 
+
+const url = "http://localhost:8080/uploads"
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [postImage, setPostImage] = useState( { myFile : ""})
+
+  const createPost = async (newImage) => {
+    try{
+      await axios.post(url, newImage)
+    }catch(error){
+      console.log(error)
+    }
+  }
+   
+  const handleSubmit =(e) =>{
+    e.preventDefault();
+  }
+  const handleFileUpload = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertToBase64(file);
+    console.log(base64)
+    setPostImage({ ...postImage, myFile : base64 })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div className="App">
+   <form onSubmit={() =>{}}>
+   <label htmlFor="file-upload" className='custom-file-upload'>
+                    <img src={postImage.myFile || avatar} alt="" />
+        </label>
+    <input 
+    type="file"
+    lable="image"
+    name="myFile"
+    id='file-upload'
+    accept='.jpeg, .png, .jpg'
+    onChange={(e) => handleFileUpload(e)} 
+    />
+
+    <h3>Simon Mburu</h3>
+    <span>Designer</span>
+    <button type='submit'>Submit</button>
+
+   </form>
+   </div>
   )
 }
 
 export default App
+
+function convertToBase64(file){
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      resolve(fileReader.result)
+    };
+    fileReader.onerror = (error) => {
+      reject(error)
+    }
+  })
+}
--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const connect = require('./database/conn.js');
-
+const cors = require('cors')
 
 //mport model
 const postSchema = require('./model/postModel.')
+const port = process.env.PORT || 8080;
+
 
 const app = express();
 // app. use() function is used to mount the specified middleware function(s) at the path which is being specified
 // express. json() function is a middleware function used in Express. js applications to parse incoming JSON data from HTTP requests, a standard format for data transmission in web servers
 app.use(express.json());
-const port = process.env.PORT || 8080;
-
+app.use(cors());
 
 
 
